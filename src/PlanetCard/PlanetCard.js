202cfff88b416bd972abfadde5f08e5c4436bf3b
Alan Charles <alanjcharles14@gmail.com>
2018-07-07 19:01:19 -0600

    Add planet card styling

--- src/PlanetCard/PlanetCard.js
+++ src/PlanetCard/PlanetCard.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import './planet-card.css';
 
 const PlanetCard = ({name, terrain, population, climate, residents}) => {
   return (
-    <div>
-      <h1>{name}</h1>
-      <h3>{terrain}</h3>
-      <h3>{population}</h3>
-      <h3>{climate}</h3>
-      <h3>{residents}</h3>
+    <div className="planet-card">
+      <div className="planet-header">
+        <h1 className="planet-name">{name}</h1>
+      </div>
+      <div className="content-div model-div"> 
+        <p className="description-title">Terrain</p>
+        <h3 className="vehicle-content">{terrain}</h3>
+      </div>
+      <hr></hr>
+      <div className="content-div"> 
+        <p className="description-title">Population</p>
+        <h3 className="vehicle-content">{population}</h3>
+      </div>
+      <hr></hr>
+      <div className="content-div"> 
+        <p className="description-title">Climate</p>
+        <h3 className="vehicle-content">{climate}</h3>
+      </div>
+      <hr></hr>
+      <div className="content-div"> 
+        <p className="description-title">Residents</p>
+        <h3 className="vehicle-content">{residents}</h3>
+      </div>
     </div>
   );
 };
