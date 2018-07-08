e8c9b7c251233da07bc880f73df55a3920bdfb37
Alan Charles <alanjcharles14@gmail.com>
2018-07-07 19:38:17 -0600

    Add buttons to vehicle and planet cards

--- src/PlanetCard/PlanetCard.js
+++ src/PlanetCard/PlanetCard.js
@@ -3,29 +3,32 @@ import './planet-card.css';
 
 const PlanetCard = ({name, terrain, population, climate, residents}) => {
   return (
-    <div className="planet-card">
-      <div className="planet-header">
-        <h1 className="planet-name">{name}</h1>
+    <div className="card-container">
+      <div className="planet-card">
+        <div className="planet-header">
+          <h1 className="planet-name">{name}</h1>
+        </div>
+        <div className="content-div model-div"> 
+          <p className="description-title">Terrain</p>
+          <h3 className="vehicle-content">{terrain}</h3>
+        </div>
+        <hr></hr>
+        <div className="content-div"> 
+          <p className="description-title">Population</p>
+          <h3 className="vehicle-content">{population}</h3>
+        </div>
+        <hr></hr>
+        <div className="content-div"> 
+          <p className="description-title">Climate</p>
+          <h3 className="vehicle-content">{climate}</h3>
+        </div>
+        <hr></hr>
+        <div className="content-div"> 
+          <p className="description-title">Residents</p>
+          <h3 className="vehicle-content">{residents}</h3>
+        </div>
       </div>
-      <div className="content-div model-div"> 
-        <p className="description-title">Terrain</p>
-        <h3 className="vehicle-content">{terrain}</h3>
-      </div>
-      <hr></hr>
-      <div className="content-div"> 
-        <p className="description-title">Population</p>
-        <h3 className="vehicle-content">{population}</h3>
-      </div>
-      <hr></hr>
-      <div className="content-div"> 
-        <p className="description-title">Climate</p>
-        <h3 className="vehicle-content">{climate}</h3>
-      </div>
-      <hr></hr>
-      <div className="content-div"> 
-        <p className="description-title">Residents</p>
-        <h3 className="vehicle-content">{residents}</h3>
-      </div>
+      <button className="favorite vehicle-favorite">Favorite</button>
     </div>
   );
 };
--- src/VehicleCard/VehicleCard.js
+++ src/VehicleCard/VehicleCard.js
@@ -3,24 +3,27 @@ import './vehicle-card.css';
 
 const VehicleCard = ({name, model, vehicleClass, passengers}) => {
   return (
-    <div className="vehicle-card">
-      <div className="vehicle-header">
-        <h1 className="vehicle-name">{name}</h1>
+    <div className="card-container">
+      <div className="vehicle-card">
+        <div className="vehicle-header">
+          <h1 className="vehicle-name">{name}</h1>
+        </div>
+        <div className="content-div model-div"> 
+          <p className="description-title">Model</p>
+          <h3 className="description-content">{model}</h3>
+        </div>
+        <hr></hr>
+        <div className="content-div"> 
+          <p className="description-title">Class</p>
+          <h3 className="description-content">{vehicleClass}</h3>
+        </div>
+        <hr></hr>
+        <div className="content-div"> 
+          <p className="description-title">Passengers</p>
+          <h3 className="description-content">{passengers}</h3>
+        </div>
       </div>
-      <div className="content-div model-div"> 
-        <p className="description-title">Model</p>
-        <h3 className="description-content">{model}</h3>
-      </div>
-      <hr></hr>
-      <div className="content-div"> 
-        <p className="description-title">Class</p>
-        <h3 className="description-content">{vehicleClass}</h3>
-      </div>
-      <hr></hr>
-      <div className="content-div"> 
-        <p className="description-title">Passengers</p>
-        <h3 className="description-content">{passengers}</h3>
-      </div>
+      <button className="favorite vehicle-favorite">Favorite</button>
     </div>
   );
 };
